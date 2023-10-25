a1e88a17bda234dafebbeb63575904c892afa82e
jatinkulkarni <jkulkarnics@utexas.edu>
2023-10-25 07:23:54 -0500

    basic framework completed

--- back-end/app.py
+++ back-end/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 # Your model and other setup code here
 class SimpleNet(nn.Module):
-    def __init__(self, input_size=821):
+    def __init__(self, input_size=819):
         super().__init__()
         self.layers = nn.Sequential(
             nn.Linear(input_size, 64),  # Modify the input size here
@@ -22,6 +22,13 @@ class SimpleNet(nn.Module):
 
     def forward(self, x):
         yhat = self.layers(x)
+        # yhat = self(x)
+        return yhat
+    
+    def predict(self, x):
+        # Pass the input through the neural network
+        with torch.no_grad():  # Ensure that no gradients are computed during prediction
+            yhat = self(x)
         return yhat
     
 model = SimpleNet()
@@ -57,7 +64,7 @@ all_months = [
 ];
 
 all_daysofmonth = [
-    'DayofMonth_1', 'DayofMonth_2', 'DayofMonth_3', 'DayofMonth_4', 'DayofMonth_5', 'DayofMonth_6', 'DayofMonth_7', 'DayofMonth_8', 'DayofMonth_9', 'DayofMonth_10', 'DayofMonth_11', 'DayofMonth_12', 'DayofMonth_13', 'DayofMonth_14', 'DayofMonth_15', 'DayofMonth_16', 'DayofMonth_17', 'DayofMonth_18', 'DayofMonth_19', 'DayofMonth_20', 'DayofMonth_21', 'DayofMonth_22', 'DayofMonth_23', 'DayofMonth_24', 'DayofMonth_25', 'DayofMonth_26', 'DayofMonth_27', 'DayofMonth_28', 'DayofMonth_29', 'DayofMonth_30'
+    'DayofMonth_1', 'DayofMonth_2', 'DayofMonth_3', 'DayofMonth_4', 'DayofMonth_5', 'DayofMonth_6', 'DayofMonth_7', 'DayofMonth_8', 'DayofMonth_9', 'DayofMonth_10', 'DayofMonth_11', 'DayofMonth_12', 'DayofMonth_13', 'DayofMonth_14', 'DayofMonth_15', 'DayofMonth_16', 'DayofMonth_17', 'DayofMonth_18', 'DayofMonth_19', 'DayofMonth_20', 'DayofMonth_21', 'DayofMonth_22', 'DayofMonth_23', 'DayofMonth_24', 'DayofMonth_25', 'DayofMonth_26', 'DayofMonth_27', 'DayofMonth_28', 'DayofMonth_29', 'DayofMonth_30', 'DayofMonth_31'
 ];
 
 all_daysofweek = [
@@ -71,6 +78,11 @@ all_originairportids = [
 all_destairportids = [
     'DestAirportID_10135', 'DestAirportID_10136', 'DestAirportID_10140', 'DestAirportID_10141', 'DestAirportID_10146', 'DestAirportID_10154', 'DestAirportID_10155', 'DestAirportID_10157', 'DestAirportID_10158', 'DestAirportID_10165', 'DestAirportID_10170', 'DestAirportID_10185', 'DestAirportID_10208', 'DestAirportID_10245', 'DestAirportID_10257', 'DestAirportID_10268', 'DestAirportID_10272', 'DestAirportID_10275', 'DestAirportID_10279', 'DestAirportID_10299', 'DestAirportID_10333', 'DestAirportID_10361', 'DestAirportID_10372', 'DestAirportID_10397', 'DestAirportID_10408', 'DestAirportID_10423', 'DestAirportID_10431', 'DestAirportID_10434', 'DestAirportID_10466', 'DestAirportID_10469', 'DestAirportID_10529', 'DestAirportID_10551', 'DestAirportID_10558', 'DestAirportID_10561', 'DestAirportID_10577', 'DestAirportID_10581', 'DestAirportID_10599', 'DestAirportID_10617', 'DestAirportID_10620', 'DestAirportID_10627', 'DestAirportID_10631', 'DestAirportID_10643', 'DestAirportID_10666', 'DestAirportID_10676', 'DestAirportID_10685', 'DestAirportID_10693', 'DestAirportID_10713', 'DestAirportID_10721', 'DestAirportID_10728', 'DestAirportID_10731', 'DestAirportID_10732', 'DestAirportID_10739', 'DestAirportID_10747', 'DestAirportID_10754', 'DestAirportID_10779', 'DestAirportID_10781', 'DestAirportID_10785', 'DestAirportID_10792', 'DestAirportID_10800', 'DestAirportID_10821', 'DestAirportID_10849', 'DestAirportID_10868', 'DestAirportID_10874', 'DestAirportID_10918', 'DestAirportID_10926', 'DestAirportID_10967', 'DestAirportID_10980', 'DestAirportID_10990', 'DestAirportID_10994', 'DestAirportID_11003', 'DestAirportID_11013', 'DestAirportID_11027', 'DestAirportID_11042', 'DestAirportID_11049', 'DestAirportID_11057', 'DestAirportID_11066', 'DestAirportID_11067', 'DestAirportID_11076', 'DestAirportID_11092', 'DestAirportID_11097', 'DestAirportID_11109', 'DestAirportID_11111', 'DestAirportID_11122', 'DestAirportID_11140', 'DestAirportID_11146', 'DestAirportID_11150', 'DestAirportID_11193', 'DestAirportID_11203', 'DestAirportID_11233', 'DestAirportID_11252', 'DestAirportID_11259', 'DestAirportID_11267', 'DestAirportID_11274', 'DestAirportID_11278', 'DestAirportID_11283', 'DestAirportID_11288', 'DestAirportID_11292', 'DestAirportID_11298', 'DestAirportID_11308', 'DestAirportID_11315', 'DestAirportID_11336', 'DestAirportID_11337', 'DestAirportID_11413', 'DestAirportID_11415', 'DestAirportID_11423', 'DestAirportID_11433', 'DestAirportID_11447', 'DestAirportID_11468', 'DestAirportID_11470', 'DestAirportID_11471', 'DestAirportID_11481', 'DestAirportID_11503', 'DestAirportID_11525', 'DestAirportID_11537', 'DestAirportID_11540', 'DestAirportID_11577', 'DestAirportID_11587', 'DestAirportID_11603', 'DestAirportID_11612', 'DestAirportID_11617', 'DestAirportID_11618', 'DestAirportID_11624', 'DestAirportID_11630', 'DestAirportID_11637', 'DestAirportID_11638', 'DestAirportID_11641', 'DestAirportID_11648', 'DestAirportID_11695', 'DestAirportID_11697', 'DestAirportID_11699', 'DestAirportID_11721', 'DestAirportID_11725', 'DestAirportID_11775', 'DestAirportID_11778', 'DestAirportID_11823', 'DestAirportID_11865', 'DestAirportID_11867', 'DestAirportID_11884', 'DestAirportID_11898', 'DestAirportID_11905', 'DestAirportID_11921', 'DestAirportID_11953', 'DestAirportID_11973', 'DestAirportID_11977', 'DestAirportID_11980', 'DestAirportID_11982', 'DestAirportID_11986', 'DestAirportID_11995', 'DestAirportID_11996', 'DestAirportID_11997', 'DestAirportID_12003', 'DestAirportID_12007', 'DestAirportID_12012', 'DestAirportID_12016', 'DestAirportID_12094', 'DestAirportID_12119', 'DestAirportID_12124', 'DestAirportID_12129', 'DestAirportID_12156', 'DestAirportID_12173', 'DestAirportID_12177', 'DestAirportID_12191', 'DestAirportID_12197', 'DestAirportID_12206', 'DestAirportID_12217', 'DestAirportID_12223', 'DestAirportID_12250', 'DestAirportID_12255', 'DestAirportID_12264', 'DestAirportID_12265', 'DestAirportID_12266', 'DestAirportID_12278', 'DestAirportID_12280', 'DestAirportID_12320', 'DestAirportID_12323', 'DestAirportID_12335', 'DestAirportID_12339', 'DestAirportID_12343', 'DestAirportID_12391', 'DestAirportID_12397', 'DestAirportID_12402', 'DestAirportID_12441', 'DestAirportID_12448', 'DestAirportID_12451', 'DestAirportID_12478', 'DestAirportID_12511', 'DestAirportID_12519', 'DestAirportID_12523', 'DestAirportID_12544', 'DestAirportID_12559', 'DestAirportID_12758', 'DestAirportID_12819', 'DestAirportID_12884', 'DestAirportID_12888', 'DestAirportID_12889', 'DestAirportID_12891', 'DestAirportID_12892', 'DestAirportID_12896', 'DestAirportID_12898', 'DestAirportID_12899', 'DestAirportID_12902', 'DestAirportID_12915', 'DestAirportID_12917', 'DestAirportID_12945', 'DestAirportID_12951', 'DestAirportID_12953', 'DestAirportID_12954', 'DestAirportID_12982', 'DestAirportID_12992', 'DestAirportID_13029', 'DestAirportID_13061', 'DestAirportID_13076', 'DestAirportID_13121', 'DestAirportID_13127', 'DestAirportID_13139', 'DestAirportID_13158', 'DestAirportID_13184', 'DestAirportID_13198', 'DestAirportID_13204', 'DestAirportID_13211', 'DestAirportID_13230', 'DestAirportID_13232', 'DestAirportID_13241', 'DestAirportID_13244', 'DestAirportID_13256', 'DestAirportID_13264', 'DestAirportID_13277', 'DestAirportID_13290', 'DestAirportID_13296', 'DestAirportID_13303', 'DestAirportID_13342', 'DestAirportID_13344', 'DestAirportID_13360', 'DestAirportID_13367', 'DestAirportID_13377', 'DestAirportID_13422', 'DestAirportID_13433', 'DestAirportID_13459', 'DestAirportID_13476', 'DestAirportID_13485', 'DestAirportID_13486', 'DestAirportID_13487', 'DestAirportID_13495', 'DestAirportID_13502', 'DestAirportID_13541', 'DestAirportID_13577', 'DestAirportID_13795', 'DestAirportID_13796', 'DestAirportID_13829', 'DestAirportID_13830', 'DestAirportID_13832', 'DestAirportID_13851', 'DestAirportID_13871', 'DestAirportID_13873', 'DestAirportID_13891', 'DestAirportID_13930', 'DestAirportID_13931', 'DestAirportID_13933', 'DestAirportID_13964', 'DestAirportID_13970', 'DestAirportID_13983', 'DestAirportID_14004', 'DestAirportID_14006', 'DestAirportID_14025', 'DestAirportID_14027', 'DestAirportID_14057', 'DestAirportID_14082', 'DestAirportID_14092', 'DestAirportID_14098', 'DestAirportID_14100', 'DestAirportID_14107', 'DestAirportID_14108', 'DestAirportID_14109', 'DestAirportID_14112', 'DestAirportID_14113', 'DestAirportID_14120', 'DestAirportID_14122', 'DestAirportID_14150', 'DestAirportID_14193', 'DestAirportID_14222', 'DestAirportID_14231', 'DestAirportID_14237', 'DestAirportID_14252', 'DestAirportID_14254', 'DestAirportID_14256', 'DestAirportID_14259', 'DestAirportID_14262', 'DestAirportID_14288', 'DestAirportID_14303', 'DestAirportID_14307', 'DestAirportID_14314', 'DestAirportID_14321', 'DestAirportID_14457', 'DestAirportID_14487', 'DestAirportID_14489', 'DestAirportID_14492', 'DestAirportID_14512', 'DestAirportID_14520', 'DestAirportID_14524', 'DestAirportID_14534', 'DestAirportID_14543', 'DestAirportID_14570', 'DestAirportID_14574', 'DestAirportID_14576', 'DestAirportID_14588', 'DestAirportID_14633', 'DestAirportID_14635', 'DestAirportID_14674', 'DestAirportID_14679', 'DestAirportID_14683', 'DestAirportID_14685', 'DestAirportID_14689', 'DestAirportID_14696', 'DestAirportID_14698', 'DestAirportID_14704', 'DestAirportID_14709', 'DestAirportID_14711', 'DestAirportID_14716', 'DestAirportID_14730', 'DestAirportID_14747', 'DestAirportID_14761', 'DestAirportID_14771', 'DestAirportID_14783', 'DestAirportID_14794', 'DestAirportID_14802', 'DestAirportID_14812', 'DestAirportID_14814', 'DestAirportID_14828', 'DestAirportID_14831', 'DestAirportID_14842', 'DestAirportID_14843', 'DestAirportID_14869', 'DestAirportID_14877', 'DestAirportID_14893', 'DestAirportID_14905', 'DestAirportID_14908', 'DestAirportID_14952', 'DestAirportID_14955', 'DestAirportID_14960', 'DestAirportID_14986', 'DestAirportID_15008', 'DestAirportID_15016', 'DestAirportID_15023', 'DestAirportID_15024', 'DestAirportID_15027', 'DestAirportID_15041', 'DestAirportID_15048', 'DestAirportID_15070', 'DestAirportID_15074', 'DestAirportID_15096', 'DestAirportID_15138', 'DestAirportID_15249', 'DestAirportID_15295', 'DestAirportID_15304', 'DestAirportID_15323', 'DestAirportID_15356', 'DestAirportID_15370', 'DestAirportID_15376', 'DestAirportID_15380', 'DestAirportID_15389', 'DestAirportID_15401', 'DestAirportID_15411', 'DestAirportID_15412', 'DestAirportID_15569', 'DestAirportID_15582', 'DestAirportID_15607', 'DestAirportID_15624', 'DestAirportID_15841', 'DestAirportID_15897', 'DestAirportID_15919', 'DestAirportID_15991', 'DestAirportID_16101', 'DestAirportID_16218', 'DestAirportID_16869'
 ]
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -101,13 +113,24 @@ def predict():
     distance = float(data['Distance'])
     taxi_out = float(data['TaxiOut'])
     wheels_off = float(data['WheelsOff'])
-    arr_del15 = float(data['ArrDel15'])
+    # arr_del15 = float(data['ArrDel15'])
     
     # Combine all features
-    features = np.array([dep_time, dep_delay, distance, taxi_out, wheels_off, arr_del15] + airline_features + month_features + dayofmonth_features + dayofweek_features + originairportid_features + destairportid_features)
+    features = np.array([dep_time, dep_delay, distance, taxi_out, wheels_off] + airline_features + month_features + dayofmonth_features + dayofweek_features + originairportid_features + destairportid_features)
+
     
+    # Convert the NumPy array to a PyTorch tensor
+    features_tensor = torch.tensor(features, dtype=torch.float32)
+
     # Make a prediction using your model
-    prediction = model.predict([features])
+    prediction = model.predict(features_tensor)
+
+    if prediction.item() == 0:
+        return {'prediction': "Flight will most likely not be delayed"}
+    else:
+        return {'prediction': "Flight will most likely be delayed"}
+
+    return {'prediction': prediction.item()}
     
     return jsonify({'prediction': prediction[0]})
 
